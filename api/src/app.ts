9736f2381db69c392b3fafe820d3a3bbbc8b1277
julia <limajulia2912@gmail.com>
2025-11-15 17:34:30 -0300

    add incident route in the 'app.ts'

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import userRoutes from "./routes/usersRoutes";
+import incidentRoutes from "./routes/incidentRoutes";
 
 const app = express();
 app.use(express.json());
 
-
 app.use("/users", userRoutes);
+app.use("/incidents", incidentRoutes);
 
 export default app;
